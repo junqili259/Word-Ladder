c1b790da1d8e3012b7789e8d94585898ae71fafc
junqili259 <39481617+junqili259@users.noreply.github.com>
2019-05-06 05:04:19 -0400

    Add files via upload

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -2,11 +2,15 @@
 #include<queue>
 #include<vector>
 #include<string>
+#include<fstream>
 
 
 
 void WordLadder::readLexicon(string input_file){
+	std::ifstream lexicon_file;
+	string word;
 
+	lexicon_file.open(input_file);
 }
 
 std::vector<string> WordLadder::findShortestLadder(string start_word, string end_word){
