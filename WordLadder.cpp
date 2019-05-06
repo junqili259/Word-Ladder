5407664fd071456770c2a8d5363987cc5fcb0cc9
junqili259 <39481617+junqili259@users.noreply.github.com>
2019-05-06 18:16:50 -0400

    Add files via upload

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -3,6 +3,8 @@
 #include<vector>
 #include<string>
 #include<fstream>
+#include<iostream>
+using std::string;
 
 
 
@@ -11,6 +13,22 @@ void WordLadder::readLexicon(string input_file){
 	string word;
 
 	lexicon_file.open(input_file);
+
+	if (lexicon_file.fail())
+	{
+		std::cerr << "Cannot read from file" << std::endl;
+	}
+	else
+		while(!lexicon_file.eof()){
+			std::getline(lexicon_file, word, '\n');
+			lexicon.push_back(word);
+		}
+		/*
+		for (int i = 0; i < lexicon.size(); i++)
+		{
+			std::cout << lexicon[i];
+		}
+		*/
 }
 
 std::vector<string> WordLadder::findShortestLadder(string start_word, string end_word){
